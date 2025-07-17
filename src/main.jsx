8175fcb6da96f4a6b739eb6030a1e3a7f15ec601
Muhammad Shafiq <muhammadshafiqchohan12@gmail.com>
2025-07-17 20:42:10 +0500

    Deploy React app

--- src/main.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';      // ✅ React component import karo
-import './App.css';           // ✅ CSS file ko sirf import karo, component nahi
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
