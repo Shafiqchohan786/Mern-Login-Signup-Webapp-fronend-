9885f2c9c121545c3c373ea5f37c6d94aeddb953
Shafiqchohan786 <muhammadshafiqchohan12@gmail.com>
2025-07-20 07:53:34 +0500

    Update Login.jsx
    Change backend link with another service

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -28,7 +28,7 @@ export default function Login() {
     setError(""); // Clear previous error
 
     try {
-      const res = await axios.post("https://webapp-backend-production-3131.up.railway.app/auth/login", {
+      const res = await axios.post("https://4stack-registration-backend.zeabur.app/auth/login", {
         email: formData.email,
         password: formData.password,
       });
