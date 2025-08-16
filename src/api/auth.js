fb7fee1e9673c1b3796c759dc119de9cdca5e49d
Shafiqchohan786 <muhammadshafiqchohan12@gmail.com>
2025-08-16 17:29:49 +0500

    Update auth.js
    changing the host backend link

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,4 +1,4 @@
-const API_BASE = "https://4stack-registration-backend.zeabur.app/auth";
+const API_BASE = "https://web-app-backend.vercel.app/auth";
 
 export const signup = (data) => axios.post(`${API_BASE}/signup`, data, { withCredentials: true });
 export const login = (data) => axios.post(`${API_BASE}/login`, data, { withCredentials: true });
